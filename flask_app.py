864b75e8a7e48b1e06e66191eeef4c86c7b0dda3
Duy Le <duy.letrong.2701@gmail.com>
2024-06-15 01:09:06 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -10,7 +10,7 @@ import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="https://vcbs-scpr.onrender.com")
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 eventlet.monkey_patch()
 
